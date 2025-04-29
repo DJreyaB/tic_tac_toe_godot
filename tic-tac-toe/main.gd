0979b8998e7343e1fd8fc60cda9f54f263b0436b
D'Jreya Boyd <37115281+DJreyaB@users.noreply.github.com>
2025-04-29 17:05:35 -0600

    main menu outlining
    created quit or restart button for the game, currently a permanent feature on bottom may add an interface menu instead

--- tic-tac-toe/main.gd
+++ tic-tac-toe/main.gd
@@ -1,1 +1,10 @@
 extends Node
+
+var game_status = false
+
+#create function for if in the game turn it to quit
+#
+func _process(delta: float) -> void:
+	if game_status:
+		$QuitRestartBtn/QuitRestartTxt.text = "Quit"
+		
